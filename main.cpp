0321f87d60524b51c79307718c935feb9d08b753
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-11-25 18:52:32 -0800

    Milestone 3

--- main.cpp
+++ main.cpp
@@ -4,25 +4,35 @@
 // Libraries
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "IntBinaryTree.h"
 using namespace std;
 
+// Constants
+const string DATA_FILE = "codes.txt";
+
+// Main function
 int main()
 {
     // Create a Binary Tree
     IntBinaryTree tree;
-    tree.insertNode("Dat");
-    tree.insertNode("Hoang");
-    tree.insertNode("Vien");
+
+    // Create file stream
+    ifstream fin;
+    fin.open(DATA_FILE);
+
+    // Create a string to store the data
+    string line;
+
+    // Read the records
+    while (getline(fin, line))
+    {
+        // Create a Node and add it to the Binary Tree
+        tree.insertNode(line);
+    }
 
     // Display the tree
     tree.displayInOrder();
-
-    // Remove a Node
-    tree.remove("Dat");
-
-    // Display the tree
-    tree.displayPreOrder();
 
     return 0;
 }
