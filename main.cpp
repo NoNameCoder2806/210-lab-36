a3b799f7c4d978469b2804b79fbee4e2fd7811d8
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-11-25 19:07:27 -0800

    Implementing a function to add a Node into the tree

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@ const string DATA_FILE = "codes.txt";
 
 // Function prototypes
 void displayMenu();
+void readData(IntBinaryTree& tree);
+void addNode(IntBinaryTree& tree);
 
 // Main function
 int main()
@@ -20,19 +22,9 @@ int main()
     // Create a Binary Tree
     IntBinaryTree tree;
 
-    // Create file stream
-    ifstream fin;
-    fin.open(DATA_FILE);
-
-    // Create a string to store the data
-    string line;
-
-    // Read the records
-    while (getline(fin, line))
-    {
-        // Create a Node and add it to the Binary Tree
-        tree.insertNode(line);
-    }
+    // Read the data
+    string path = DATA_FILE;
+    readData(tree, path);
 
     // Display the tree
     tree.displayInOrder();
@@ -50,12 +42,67 @@ int main()
 void displayMenu()
 {
     // Display a header
-    cout << "";
-    
+    cout << " --- Operation Menu --- " << endl;
+
     // Display all the operations
     cout << "1. Add Node" << endl;
     cout << "2. Delete Note" << endl;
     cout << "3. Search Node" << endl;
     cout << "4. Modify Records" << endl;
     cout << "5. Exit" << endl;
+}
+
+/*
+    readData()
+    Read the data from a file and insert the Nodes into the Binary Tree
+    Arguments:
+        - tree: the Binary Tree to insert to
+        - path: the path of the data file
+    Return: none
+*/
+void readData(IntBinaryTree& tree, string path)
+{
+    // Create file stream
+    ifstream fin;
+    fin.open(path);
+
+    // Create a string to store the data
+    string line;
+
+    // Read the records
+    while (getline(fin, line))
+    {
+        // Create a Node and add it to the Binary Tree
+        tree.insertNode(line);
+    }
+}
+
+/*
+    addNode()
+    Add a Node into the Binary Tree
+    Arguments:
+        - tree: the Binary Tree to insert to
+    Return: none
+*/
+void addNode(IntBinaryTree& tree)
+{
+    // Create a string variable
+    string str;
+
+    // Prompt the user to enter a string
+    cout << "Please enter a string to insert: ";
+    cin >> str;
+    cin.ignore(1000, 10);
+
+    // Check the length of the Node
+    if (str.length() == 0)
+    {
+        // Display an error message
+        cout << "Error! The string must not be empty!" << endl;
+
+        // Prompt the user to enter a new string
+        cout << "Please enter a new string: ";
+        cin >> str;
+        cin.ignore(1000, 10);
+    }
 }
